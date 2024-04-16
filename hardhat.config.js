683e083b2a7a64d1669d337c669ec047b5bd2429
helloram007 <161843181+helloram007@users.noreply.github.com>
2024-04-17 00:21:39 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,7 +13,6 @@ module.exports = {
       url: SEPOLIA_URL || "",      
       accounts: 
         SECRET_KEY !== undefined ? [SECRET_KEY] : [],
-      //["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", ]
     },
   }
 };
