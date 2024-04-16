c5310d1f8110d288a427a8f9ad683ab029f3a09e
Ram Chandhar Rapolu <ramfx80@gmail.com>
2024-04-16 15:08:24 +0100

    adding my first hardhat project, i have added installation script, so if it goes wrong, you can update it again avoiding manual tasks

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const { SEPOLIA_URL, SECRET_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.24",
+};
---
+++ install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+## install nvm package
+## choose the latest version
+NVM_VERSION="v0.39.7"
+wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh" | bash
+
+## install nodejs and pick the latest LTS version
+nvm install --lts
+
+## now the installation of nodejs is done, 
+## now you can start your nodejs project development
+
+## Initialise npm project
+#First create a directory for your project
+PROJECT_NAME="hardhat-testing"
+mkdir "$PROJECT_NAME"
+cd $PROJECT_NAME
+
+## Initialise npm project
+## This will create a package.json file
+npm init -y
+
+## now initialise(package-lock.json)
+npm i -y
+
+##Now install hardhat
+npm install --save-dev hardhat
+
+##now create a project from hardhat defaults
+## choose the defaults when prompted.
+##This will create a project
+npx hardhat init
+
+## compile and test the default contract 
+##This commands(compile) will create artifacts directory
+npx hardhat compile
+
+## now test
+npx hardhat test
+##if you want to test only a single, then you can use
+## npx hardhat test <testfilename>
+
+## if you want to clean hardhat directory
+## this will delete the artifacts directory nad you can compile to generate again.
+npx hardhat clean
+
+## if you want to install env config library
+## this will be used to hide your important data.
+## create .env file to store all import and hide this from users.
+
+npm install dotenv && touch .env
+
+## if you want to install openzeppelin contracts
+npm install @openzeppelin/contracts
+## if you want to upgradeable files of thiss verions.
+npm install @openzeppelin/contracts-upgradeable @openzeppelin/contracts
+
+
+
+
+
+
+
+
+
